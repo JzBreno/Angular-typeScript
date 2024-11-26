3138d1506c16f1f78a578d3c82f6ace012d6bb19
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-11-26 20:03:43 -0300

    forms angular

--- src/app/componente05diretiva-ngfor/componente05diretiva-ngfor.component.ts
+++ src/app/componente05diretiva-ngfor/componente05diretiva-ngfor.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-componente05diretiva-ngfor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './componente05diretiva-ngfor.component.html',
   styleUrl: './componente05diretiva-ngfor.component.css'
 })
 export class Componente05diretivaNgforComponent {
-  nomes: string[] = ['Breno','Jose','Antonio','Maria','Leo'];
 
+  detectoras = ['1', '2', '3', '4', '5']; // Lista de detectoras
+  selectedDetectoras: string[] = []; // Armazena as selecionadas
 
+  // Método para capturar mudanças no checkbox
+  onCheckboxChange(event: Event) {
+    const input = event.target as HTMLInputElement; // Obtém o checkbox
+    const value = input.value;
+
+    if (input.checked) {
+      // Adiciona à lista se estiver marcado
+      this.selectedDetectoras.push(value);
+    } else {
+      // Remove da lista se estiver desmarcado
+      this.selectedDetectoras = this.selectedDetectoras.filter(det => det !== value);
+    }
+
+    console.log(this.selectedDetectoras); // Debug: verificar quais estão selecionadas
+  }
 }
